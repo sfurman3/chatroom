160f3a550386abde1741166d69feeed92080be81
seth <furman.seth@gmail.com>
2017-09-30 14:11:15 -0400

    Add updated grading script

--- grading.py
+++ grading.py
@@ -7,7 +7,8 @@ import shutil
 os.system('./build')
 
 test_output = 'test_output'
-tests = 'tests'
+tests = 'grading_tests'
+output = 'results_1'
 if len(sys.argv) == 2:
     tests = sys.argv[1]
 try:
@@ -15,11 +16,20 @@ try:
 except:
     pass
 os.mkdir(test_output)
+list_of_tests = [];
 for f in os.listdir(tests):
+	list_of_tests.append(f)
+list_of_tests.sort()
+
+print list_of_tests
+output_file = open(output, 'w')
+
+for f in list_of_tests:
     abs_f = join(tests, f)
     if isfile(abs_f):
         if f[len(f) - len('.input'):] == '.input':
             fn = f[:len(f) - len('.input')]
+            output_file.write(fn + ' ')
             print fn,
             os.system('./master.py < ' + abs_f + \
                       ' 2> ' + join(test_output, fn+'.err') + \
@@ -30,7 +40,9 @@ for f in os.listdir(tests):
             with open(join(tests, fn+'.output')) as fi:
                 std = fi.read()
             if out.strip() == std.strip():
+            	output_file.write('correct \n')
                 print 'correct'
             else:
+            	output_file.write('wrong \n')
                 print 'wrong'
             time.sleep(2)
